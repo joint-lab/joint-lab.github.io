244323f0b88fe38a54bcc845bd9109ed66338534
Edward Laurence <edwardl@hectiq.ai>
2021-12-10 17:05:40 -0500

    Update the front logo

--- src/components/core/icons.js
+++ src/components/core/icons.js
@@ -42,29 +42,38 @@ export function OGYIcon(){
           </svg>
 }
 
-
 export function JointIcon({ dark }){
 
   return <svg
-            className="transform scale-90 translate-x-8 -translate-y-6 lg:-translate-y-12 overflow-visible "
+            className="transform scale-90 rotate-12 translate-x-8 -translate-y-6 lg:-translate-y-12 overflow-visible "
             width={100}
             height={100}
             fill="none"
             viewBox="0 0 100 100"
           >
-              <circle cx="25" cy="50" r="60" fill={dark? "#31a354":"white"} opacity="0.1" className=""/>
-              <circle cx="25" cy="50" r="80" fill={dark? "#31a354":"white"} opacity="0.1" className=""/>
-              <circle cx="32" cy="50" r="80" fill={dark? "#31a354":"white"} opacity="0.1" className="animate-ping-slow origin-center"/>
+              <circle cx="60" cy="50" r="90" fill={dark? "#31a354":"white"} opacity="0.1" className=""/>
+              <circle cx="60" cy="50" r="120" fill={dark? "#31a354":"white"} opacity="0.1" className=""/>
 
-              <circle cx="10" cy="20" r="6"  fill="yellow" className="animate-ping-slow origin-right "/>
-              <circle cx="60" cy="50" r="6"  fill="yellow" className="animate-ping-xxslow origin-left "/>
-              <circle cx="10" cy="80" r="6"  fill="yellow" className="animate-ping-xslow origin-right "/>
+              <circle cx="60" cy="50" r="120" stroke={dark? "#31a354":"white"}  opacity="0.8" className="animate-ping-xslow origin-center"/>
+              <circle cx="60" cy="50" r="120" stroke={dark? "#31a354":"white"} opacity="0.8" className="animate-ping-slow origin-center"/>
+
+              <circle cx="10" cy="20" r="6" strokeWidth="2" stroke="white" fill="#fed976" className="animate-ping-fast origin-right "/>
+              <circle cx="10" cy="80" r="6" strokeWidth="2" stroke="white" fill="#fed976" className="animate-ping-xxxslow origin-right "/>
+              <circle cx="110" cy="20" r="6" strokeWidth="2" stroke="white" fill="#fed976" className="animate-ping-xxslow origin-right "/>
+              <circle cx="110" cy="80" r="6" strokeWidth="2" stroke="white" fill="#fed976" className="animate-ping-xslow origin-right "/>
 
               <circle cx="10" cy="20" r="6" stroke={dark? "#00441b":"white"} strokeWidth="2" fill="none"/>
               <circle cx="60" cy="50" r="6" stroke={dark? "#00441b":"white"} strokeWidth="2" fill="none"/>
               <circle cx="10" cy="80" r="6" stroke={dark? "#00441b":"white"} strokeWidth="2" fill="none"/>
+              <circle cx="110" cy="20" r="6" stroke={dark? "#00441b":"white"} strokeWidth="2" fill="none"/>
+              <circle cx="110" cy="80" r="6" stroke={dark? "#00441b":"white"} strokeWidth="2" fill="none"/>
+    
               <path d="M 10,75 10,25" stroke={dark? "#00441b":"white"} strokeWidth="2" />
+              <path d="M 110,75 110,25" stroke={dark? "#00441b":"white"} strokeWidth="2" />
               <path d="M 14,77 56,53" stroke={dark? "#00441b":"white"} strokeWidth="2" />
               <path d="M 14,24 56,47" stroke={dark? "#00441b":"white"} strokeWidth="2" />
+              <path d="M 107,25 64,47" stroke={dark? "#00441b":"white"} strokeWidth="2" />
+              <path d="M 107,75 64,53" stroke={dark? "#00441b":"white"} strokeWidth="2" />
+              
           </svg>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       animation: {
         'pulse-slow': 'pulse-slow 5s cubic-bezier(0.4, 0, 0.6, 1) infinite;',
         'pulse-linear': 'pulse-slow 5s linear infinite;',
-        'ping-slow': 'ping 4.2s cubic-bezier(0, 0, 0.2, 1) infinite;',
+        'ping-fast': 'ping 2.2s cubic-bezier(0, 0, 0.2, 0.2) infinite;',
+        'ping-slow': 'ping 4.2s cubic-bezier(0, 0, 0.2, 0.2) infinite;',
         'ping-xslow': 'ping 6.1s cubic-bezier(0, 0, 0.2, 1) infinite;',
         'ping-xxslow': 'ping 5.5s cubic-bezier(0, 0, 0.2, 1) infinite;',
         'ping-xxslow-finite': 'ping-finite 3s cubic-bezier(0, 0.2, 0.5, 1);',
