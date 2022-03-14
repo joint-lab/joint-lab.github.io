035511fc3f075b5b0575e33a656b4bfac2831938
Edward Laurence <edwardl@hectiq.ai>
2022-03-13 20:42:32 -0400

    Hotfix

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,20 +81,5 @@ module.exports = {
       },
       __key: "pages",
     },
-    {
-      resolve: 'gatsby-plugin-favicons',
-      options: {
-        logo: './src/images/favicon.png',
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          yandex: false,
-          windows: false
-        }
-      }
-    }
   ],
 };
